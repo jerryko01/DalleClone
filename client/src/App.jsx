2a690d6934589d260efe15d53ddb8030ec4a50dd
jerryko01 <jerrytake01@gmail.com>
2023-07-03 23:27:11 -0400

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { logo } from "./assets";
+import './App.css'
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <header className></header>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,7 @@
+const Home = () => {
+    return (
+        <div>Create Post</div>
+    )
+}
+
+export default Home;
