f3f2923d6f4fe4a2b6567f76e89dea397ee8a17f
jerryko01 <jerrytake01@gmail.com>
2023-08-16 00:38:53 -0400

    Finished the Project - everything works well

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Loader, Card, FormField } from "../components";
 
 const RenderCards = ({ data, title }) => {
-    if (data.length > 0) return data.map((post) => <Card key={post.id} {...post} />)
+    if (data?.length > 0) return data.map((post) => <Card key={post.id} {...post} />)
     return (
         <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
     )
@@ -12,6 +12,45 @@ const Home = () => {
     const [loading, setLoading] = useState(false);
     const [allPosts, setAllPosts] = useState(null);
     const [searchText, setSearchText] = useState("");
+    const [searchedResults, setSearchedResults] = useState(null);
+    const [searchTimeout, setSearchTimeout] = useState(null);
+
+    useEffect(() => {
+        const fetchPosts = async () => {
+            setLoading(true);
+            try {
+                const response = await fetch("http://localhost:8080/api/v1/post", {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json",
+                    }
+                })
+                if (response.ok) {
+                    const result = await response.json();
+                    setAllPosts(result.data.reverse())
+                }
+            } catch (error) {
+                alert(error)
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchPosts();
+    }, [])
+
+    const handleSearchChange = (e) => {
+        clearTimeout(searchTimeout)
+        setSearchText(e.target.value);
+        setSearchTimeout(
+            setTimeout(() => {
+                const searchResults = allPosts.filter((item) =>
+                    item.name.toLowerCase().includes(searchText.toLowerCase()) || item.prompt.toLowerCase().includes(searchText.toLowerCase())
+                )
+                setSearchedResults(searchResults)
+            }, 500)
+        )
+    }
+
     return (
         <section className="max-w-7xl mx-auto flex flex-col">
             <div>
@@ -20,7 +59,14 @@ const Home = () => {
             </div>
 
             <div className="mt-10">
-                <FormField />
+                <FormField
+                    LabelName=""
+                    type="text"
+                    name="text"
+                    placeholder="Search posts"
+                    value={searchText}
+                    handleChange={handleSearchChange}
+                />
             </div>
 
             <div className="mt-10">
@@ -38,9 +84,9 @@ const Home = () => {
                             )}
                             <div className="grid lg:grid-cols-4 sm:grid-cols-3 xs:grid-cols-2 grid-cols-1 gap-3">
                                 {searchText ? (
-                                    <RenderCards data={[]} title="No search results found" />
+                                    <RenderCards data={searchedResults} title="No search results found" />
                                 ) : (
-                                        <RenderCards data={[]} title="No posts found" />
+                                        <RenderCards data={allPosts} title="No posts found" />
                                     )}
                             </div>
                         </>
